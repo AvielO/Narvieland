69f22b77330670b4210a88423b472a3b284ee274
AvielO <Aviell1100611@gmail.com>
2020-08-19 23:55:41 +0300

    Added auth module and login Form

---
+++ client/src/app/authentication/authentication.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [SigninComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    FormsModule,
+    SharedModule
+  ]
+})
+export class AuthenticationModule { }
---
+++ client/src/app/authentication/signin/signin.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+  constructor() { }
+  username: string;
+  password: string;
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    
+  }
+}
