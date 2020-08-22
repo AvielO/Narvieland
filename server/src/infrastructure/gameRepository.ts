d04719166b96f00b0c814e77b7d3136e491d43fa
Aviel Ovadiya <aviell1100611@gmail.com>
2020-08-22 17:08:09 +0300

    latest version

--- server/src/infrastructure/gameRepository.ts
+++ server/src/infrastructure/gameRepository.ts
@@ -1,6 +1,10 @@
 import GameRepo from '../core/repository';
+import mongoose from 'mongoose';
 
 export class SqlGameRepo implements GameRepo {
+  SERVER = '127.0.0.1:27017';
+  DATABASE = 'NarvieslandDB';
+
   signin = async (username: string, password: string): Promise<boolean> => {
     try {
       return false;
--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import swaggerui from '../swaggerRouter';
 import exampleApi from '../api/authentications';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const API_PREFIX = '/api/v1';
 
@@ -11,8 +12,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(API_PREFIX, exampleApi);
+
+mongoose.connect('mongodb://localhost:27017/NarvieslandDB', { useNewUrlParser: true }, error => {
+  if (!error) {
+    console.log('success');
+  } else {
+    console.log('Failed');
+  }
+});
 
 app.get('/', (req, res) => res.redirect('/swagger'));
 app.use('/swagger', swaggerui);
